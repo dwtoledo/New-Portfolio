502c85eff45ef9d4aaacc13c4981b971057c3ed8
Douglas Toledo <dwtoledo@outlook.com>
2023-10-20 14:30:51 -0400

    Add Favicons
    Remove Preconnected Google Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,10 +70,6 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
-      fontFamily: {
-        title: ['"Oswald"', 'sans-serif'],
-        text: ["'Source Serif 4'", 'serif'],
-      },
     },
   },
   plugins: [require('tailwindcss-animate')],
