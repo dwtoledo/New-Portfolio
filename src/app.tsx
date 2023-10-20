dae77ad91bb94ed9362f4b414cdf2c20ac6e19d8
Douglas Toledo <dwtoledo@outlook.com>
2023-10-20 11:29:53 -0400

    Install Shadcn UI
    Add Initial ToogleMode to switch between dark and light mode
    Add and configure Font Families on TailwindCSS

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from './components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
+import './index.css'
+
 export function App() {
-  return <h1>Hello New Portfolio</h1>
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <h1>Hello New Portfolio</h1>
+      <ModeToggle />
+    </ThemeProvider>
+  )
 }
