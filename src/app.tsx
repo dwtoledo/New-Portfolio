bb347123a177eef190e60469a8c974e42cb48e72
Douglas Toledo <dwtoledo@outlook.com>
2023-10-20 20:49:04 -0400

    Implement Get GitHub Profile Data function on App
    Implement GitHub Profile Context Provider on App

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react'
 import { ThemeProvider } from './components/theme-provider'
 import { Header } from './components/Header'
-import './index.css'
 import { Separator } from './components/ui/separator'
+import { Profile } from './components/Profile'
+
+import { GitHubProfileContext } from './contexts/github-profile'
+
+import './index.css'
 
 export function App() {
+  const [profile, setProfile] = useState({})
+
+  useEffect(() => {
+    getGitHubProfileData()
+  }, [])
+
+  async function getGitHubProfileData() {
+    await fetch('https://api.github.com/users/dwtoledo').then(response =>
+      response.json().then(data => {
+        setProfile(data)
+      }),
+    )
+  }
+
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <Header />
-      <Separator />
+      <GitHubProfileContext.Provider value={{ profile, setProfile }}>
+        <Header />
+        <Separator />
+        <Profile />
+      </GitHubProfileContext.Provider>
     </ThemeProvider>
   )
 }
