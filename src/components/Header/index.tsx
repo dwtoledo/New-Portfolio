009fa272d560ec6020bb16506cf2b3fb87681f7a
Douglas Toledo <dwtoledo@outlook.com>
2023-10-20 20:26:53 -0400

    Improve Header with Avatar and GitHub Profile Context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
-import { Button } from '@/components/ui/button'
+import { useContext } from 'react'
 import { Github, Linkedin, Mail } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { ModeToggle } from '../mode-toggle'
 
+import { GitHubProfileContext } from '@/contexts/github-profile'
+
 export function Header() {
+  const { profile } = useContext(GitHubProfileContext)
+
   return (
     <header className="container flex flex-col py-4 items-center gap-y-2 sm:flex-row justify-between">
-      <span className="text-3xl font-extrabold">Douglas Toledo</span>
+      <div className="flex gap-x-2 items-center">
+        <Avatar>
+          <AvatarImage src={profile.avatar_url} />
+          <AvatarFallback>{profile.name || 'Douglas Toledo'}</AvatarFallback>
+        </Avatar>
+        <span className="text-xl font-extrabold">
+          {profile.name || 'Douglas Toledo'}
+        </span>
+      </div>
+
       <div className="flex gap-x-2">
         <Button asChild>
           <a
@@ -29,7 +44,9 @@ export function Header() {
         </Button>
         <Button variant="secondary">
           <a
-            href="https://github.com/dwtoledo"
+            href={`https://github.com/${
+              profile.login ? profile.login : 'dwtoledo'
+            }`}
             target="_blank"
             rel="noreferrer"
           >
