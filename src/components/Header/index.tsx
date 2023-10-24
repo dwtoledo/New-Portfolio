4c8dafb7f3f86c6b7e4342787e6dcc9439168dc1
Douglas Toledo <dwtoledo@outlook.com>
2023-10-24 13:09:22 -0400

    Correct Border Bottom on Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,45 +11,51 @@ export function Header() {
   const { profile } = useContext(GitHubProfileContext)
 
   return (
-    <header className="container bg-background border-b flex flex-col py-4 items-center gap-y-2 sm:flex-row justify-between sticky top-0 z-10">
-      <div className="flex gap-x-2 items-center">
-        <Avatar>
-          <AvatarImage src={profile.avatar_url || avatarImg} />
-          <AvatarFallback>{profile.name || 'Douglas Toledo'}</AvatarFallback>
-        </Avatar>
-        <span className="text-xl font-extrabold">
-          {profile.name || 'Douglas Toledo'}
-        </span>
-      </div>
+    <header className="bg-background border-b py-4 sm:sticky top-0 z-10">
+      <div className="container flex flex-col items-center gap-y-2 sm:flex-row justify-between">
+        <div className="flex gap-x-2 items-center">
+          <Avatar>
+            <AvatarImage src={profile.avatar_url || avatarImg} />
+            <AvatarFallback>{profile.name || 'Douglas Toledo'}</AvatarFallback>
+          </Avatar>
+          <span className="text-xl font-extrabold">
+            {profile.name || 'Douglas Toledo'}
+          </span>
+        </div>
 
-      <div className="flex gap-x-2">
-        <a href="mailto:dwtoledo@outlook.com" target="_blank" rel="noreferrer">
-          <Button className="flex gap-2 items-center">
-            <Mail className="h-[1.2rem] w-[1.2rem]" />
-            <span className="hidden sm:inline">Send me an email</span>
-          </Button>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/dwtoledo/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button>
-            <Linkedin className="h-[1.2rem] w-[1.2rem]" />
-          </Button>
-        </a>
-        <a
-          href={`https://github.com/${
-            profile.login ? profile.login : 'dwtoledo'
-          }`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button>
-            <Github className="h-[1.2rem] w-[1.2rem]" />
-          </Button>
-        </a>
-        <ModeToggle />
+        <div className="flex gap-x-2">
+          <a
+            href="mailto:dwtoledo@outlook.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button className="flex gap-2 items-center">
+              <Mail className="h-[1.2rem] w-[1.2rem]" />
+              <span className="hidden sm:inline">Send me an email</span>
+            </Button>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dwtoledo/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button>
+              <Linkedin className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+          </a>
+          <a
+            href={`https://github.com/${
+              profile.login ? profile.login : 'dwtoledo'
+            }`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button>
+              <Github className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+          </a>
+          <ModeToggle />
+        </div>
       </div>
     </header>
   )
