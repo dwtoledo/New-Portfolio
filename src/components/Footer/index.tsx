1dedb598e0003b58833ea3ef4719bf33c51654f0
Douglas Toledo <dwtoledo@outlook.com>
2023-10-29 16:49:14 -0400

    Update Footer Component with Technology used

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,10 @@ export function Footer() {
   return (
     <footer className="container py-5 px-10 flex gap-2 items-center justify-center">
       <Terminal size={16} className="text-primary" strokeWidth="3" />
-      <span>Developed by Douglas Toledo, {new Date().getFullYear()}</span>
+      <code>
+        {new Date().getFullYear()}, Developed by Douglas Toledo using{' '}
+        <strong>React</strong>
+      </code>
     </footer>
   )
 }
