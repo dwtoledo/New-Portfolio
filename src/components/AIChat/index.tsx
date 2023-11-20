513e221596645bd12d821b7ed57bba77153b8094
Douglas Toledo <dwtoledo@outlook.com>
2023-11-19 22:18:01 -0500

    Improve and Style AIChat component

--- src/components/AIChat/index.tsx
+++ src/components/AIChat/index.tsx
@@ -1,10 +1,22 @@
-import { GitHubProfileContext } from '@/contexts/github-profile'
+import { FormEvent, useContext, useEffect, useRef, useState } from 'react'
+import { GitHubProfileContext } from '../../contexts/github-profile'
 import { OpenAI } from 'openai'
 import { ChatCompletionMessageParam } from 'openai/resources/chat/index.mjs'
-import { FormEvent, useContext, useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown, { Components } from 'react-markdown'
+import { Loader2, Send } from 'lucide-react'
+import genericAvatar from '../../assets/images/generic-avatar.svg'
 
-import './style.css'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input"
 
 const openai = new OpenAI({
   organization: import.meta.env.VITE_OPENAI_ORGANIZATION_ID,
@@ -47,15 +59,56 @@ EDUCATION:
 LANGUAGES:
   • English and Portuguese.`
 
+const customReactMarkdownComponent: Partial<Components> = {
+  a: ({ node, children, ...props }) => {
+    if (props.href?.includes('http')) {
+      props.target = '_blank'
+      props.rel = 'noopener noreferrer'
+    }
+    return (
+      <a
+        style={{
+          color: '#003cff',
+          fontWeight: 'bold',
+          textDecoration: 'underline',
+        }}
+        {...props}
+      >
+        {children}
+      </a>
+    )
+  },
+}
 
 export function AIChat() {
-  const { repos } = useContext(GitHubProfileContext)
-
   const [message, setMessage] = useState('')
   const [chats, setChats] = useState<Array<ChatCompletionMessageParam>>([])
   const [isTyping, setIsTyping] = useState(false)
+  const { repos, profile } = useContext(GitHubProfileContext)
+  const chatWrapper = useRef<HTMLUListElement>(null)
 
-  function convertGitHubReposInText() {
+  useEffect(() => {
+    observeChatMessages()
+  }, [])
+
+  function observeChatMessages() {
+    const observer = new MutationObserver(mutationList =>
+      mutationList.filter(mutation => mutation.type === 'childList').forEach(mutation => {
+        mutation.addedNodes.forEach(() => {
+          if (chatWrapper.current) {
+            const { current } = chatWrapper;
+            if (current.children.length > 0) {
+              const lastChild = current.children[current.children.length - 1] as HTMLElement;
+              lastChild.scrollIntoView({ behavior: "smooth", block: "nearest" });
+            }
+          }
+        });
+      }));
+
+    if (chatWrapper.current) observer.observe(chatWrapper.current, { childList: true, subtree: true });
+  }
+
+  function convertReposInText() {
     if (!repos.length) return 'Error on load my GitHub projects'
     let convertion: Array<any> = []
     repos.forEach((repo, index) => {
@@ -66,17 +119,6 @@ export function AIChat() {
       )
     })
     return convertion.join('; ')
-  }
-
-  function createPromptMessages(messages: ChatCompletionMessageParam[]) {
-    const principalMessage: ChatCompletionMessageParam = {
-      role: 'user',
-      content: `Meu nome é Douglas, sou desenvolvedor front-end. Abaixo segue meu currículo e alguns projetos pessoais no Github, ambos estão em inglês. As próximas mensagens serão perguntas de recrutadores. Por favor, responda como se fosse eu, seja objetivo e profissional, insira link para o projeto do github caso a resposta cite o projeto, usando o mesmo idioma da pergunta. Responda as perguntas apenas se ela estiver relacionada ao meu perfil profissional, experiência de trabalho, habilidades técnicas ou projetos no Github. A pergunta deve contribuir para a avaliação de minha adequação para um processo seletivo ou estar diretamente relacionada ao meu currículo. Exemplo de pergunta aceitável: 'Qual foi a conquista profissional mais significativa para o Douglas'. Exemplo de pergunta não aceitável: 'Conte-me uma piada de elefante'." Não reponder que trabalhei profissionalmente com uma tecnologia que o recrutador perguntou e que não consta no meu currículo. A resposta deve estar em markdown. O meu currículo é: '''${myProfessionalBackground}'''. Os meus projetos no GitHub são: '''${convertGitHubReposInText()}'''`,
-    }
-    if (chats.length < 3) {
-      return [principalMessage, ...messages]
-    }
-    return [principalMessage, ...chats.slice(-1)]
   }
 
   async function chat(event: FormEvent, message: string) {
@@ -95,7 +137,7 @@ export function AIChat() {
       .create({
         model: 'gpt-3.5-turbo',
         temperature: 0,
-        messages: createPromptMessages(messages),
+        messages: createMessagesRequest(messages),
       })
       .then(response => {
         messages.push(response.choices[0].message)
@@ -107,54 +149,106 @@ export function AIChat() {
       })
   }
 
+  function createMessagesRequest(messages: ChatCompletionMessageParam[]) {
+    const principalMessage: ChatCompletionMessageParam = {
+      role: 'user',
+      content: `Meu nome é Douglas, sou desenvolvedor front-end. Abaixo segue meu currículo e alguns projetos pessoais no Github, ambos estão em inglês. As próximas mensagens serão perguntas de recrutadores. Por favor, responda como se fosse eu, seja objetivo e profissional, insira link para o projeto do github caso a resposta cite o projeto, usando o mesmo idioma da pergunta. Responda as perguntas apenas se ela estiver relacionada ao meu perfil profissional, experiência de trabalho, habilidades técnicas ou projetos no Github. A pergunta deve contribuir para a avaliação de minha adequação para um processo seletivo ou estar diretamente relacionada ao meu currículo. Não reponder que trabalhei profissionalmente com uma tecnologia que o recrutador perguntou e que não consta no meu currículo. A resposta deve estar em markdown. O meu currículo é: '''${myProfessionalBackground}'''. Os meus projetos no GitHub são: '''${convertReposInText()}'''`,
+    }
+    if (chats.length < 3) {
+      return [principalMessage, ...messages]
+    }
+    return [principalMessage, ...chats.slice(-1)]
+  }
+
+  function getInputFormButton() {
+    if (isTyping) {
+      return (
+        <Button type="submit" disabled={isTyping} className='w-32 flex gap-2'>
+          <Loader2 className="h-4 w-4 animate-spin" />
+          <span>Thinking... </span>
+        </Button>)
+    } else {
+      return (
+        <Button type="submit" disabled={isTyping} className='w-32 flex gap-2'>
+          <Send className="h-4 w-4" />
+          <span>Send</span>
+        </Button>)
+    }
+  }
+
+  function createChatMessage(chat: ChatCompletionMessageParam, index: number) {
+    switch (chat.role) {
+      case 'user':
+        return (
+          <li key={index} className='self-end'>
+            <div className='flex items-start gap-4'>
+              <div className='flex flex-col items-end'>
+                <span className='font-extrabold text-primary'>You:</span>
+                <ReactMarkdown className="leading-relaxed"
+                  components={customReactMarkdownComponent}
+                  children={chat.content}
+                />
+              </div>
+              <Avatar className='h-10 w-10 border-2	border-border'>
+                <AvatarImage src={genericAvatar} />
+                <AvatarFallback>User</AvatarFallback>
+              </Avatar>
+            </div>
+          </li>
+        )
+      case 'assistant':
+        return (
+          <li key={index}>
+            <div className='flex items-start gap-4'>
+              <Avatar className='h-10 w-10 border-2	border-border'>
+                <AvatarImage src={profile.avatar_url} />
+                <AvatarFallback>AI Assistant</AvatarFallback>
+              </Avatar>
+              <div className='flex flex-col'>
+                <span className='font-extrabold text-primary'>
+                  AI Assistant:
+                </span>
+                <ReactMarkdown className="leading-relaxed"
+                  components={customReactMarkdownComponent}
+                  children={chat.content}
+                />
+              </div>
+            </div>
+          </li>
+        )
+      default:
+        return ''
+    }
+  }
+
   return (
-    <div>
-      {isTyping ? 'typing' : null}
-      <form action="" onSubmit={event => chat(event, message)}>
-        <input
-          type="text"
-          name="message"
-          value={message}
-          placeholder="Type a message here and hit Enter..."
-          onChange={e => setMessage(e.target.value)}
-          disabled={isTyping}
-        />
-      </form>
-      <section>
-        {chats && chats.length
-          ? chats.map((chat, index) => (
-            <p key={index} className={chat.role === 'user' ? 'user_msg' : ''}>
-              <span>
-                <b>{chat.role.toUpperCase()}</b>
-              </span>
-              <span>:</span>
-              <ReactMarkdown
-                components={{
-                  a: ({ node, children, ...props }) => {
-                    if (props.href?.includes('http')) {
-                      props.target = '_blank'
-                      props.rel = 'noopener noreferrer'
-                    }
-                    return (
-                      <a
-                        style={{
-                          color: '#003cff',
-                          fontWeight: 'bold',
-                          textDecoration: 'underline',
-                        }}
-                        {...props}
-                      >
-                        {children}
-                      </a>
-                    )
-                  },
-                }}
-                children={String(chat.content)}
-              />
-            </p>
-          ))
-          : ''}
-      </section>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Chat with our AI Assistant!</CardTitle>
+        <CardDescription>
+          Ask about my profile, experience, and projects!
+          <br /> Please confirm the AI responses with a Douglas interview.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='max-h-80 overflow-y-scroll'>
+        <ul className='flex flex-col gap-4' ref={chatWrapper}>
+          {chats?.length ? chats.map((chat, index) => createChatMessage(chat, index)) : null}
+          <div className='mt-2'></div>
+        </ul>
+      </CardContent>
+      <CardFooter>
+        <form onSubmit={event => chat(event, message)} className='flex gap-2 w-full'>
+          <Input
+            type="text"
+            className='flex-1'
+            placeholder="Type your question..."
+            value={message}
+            onChange={event => setMessage(event.target.value)}
+            disabled={isTyping}
+          />
+          {getInputFormButton()}
+        </form>
+      </CardFooter>
+    </Card >
   )
 }
