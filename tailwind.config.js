74540865bfcbf831c22bcb7f34d0f16399f7d518
Douglas Toledo <dwtoledo@outlook.com>
2023-11-30 17:04:22 -0500

    Add complementary color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,10 @@ module.exports = {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
+        complementary: {
+          DEFAULT: 'hsl(var(--complementary))',
+          foreground: 'hsl(var(--complementary-foreground))',
+        },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
